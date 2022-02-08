8b5cd621e7ce4ff0e6dba97edf199ee809271eed
JMIKEPAD <mchparodi@gmail.com>
2022-02-08 13:27:18 +0100

    update

---
+++ parseStringToArray.js
@@ -0,0 +1,13 @@
+class ParseStringToArrayofNumber{
+    constructor(string){
+        this.string = string;
+        this.array = [];
+
+    }
+
+sliceSemicolon(string) {
+    const slicedString = this.string.split(";");
+    return slicedString
+}
+// slisceBlanckSpace()
+// }
